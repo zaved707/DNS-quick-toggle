fad4c6999d68f57e375da76fbf64c6834aee306c
Zaved <zavedahmadca707@gmail.com>
2025-05-17 01:40:30 +0530

    added delete icon

--- app/src/main/java/com/zavedahmad/dnstoggle/ui/activities/MainActivity.kt
+++ app/src/main/java/com/zavedahmad/dnstoggle/ui/activities/MainActivity.kt
@@ -7,48 +7,42 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
+
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
+
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
+
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.material3.Button
-import androidx.compose.material3.Card
+
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.LargeFloatingActionButton
-import androidx.compose.material3.LargeTopAppBar
+
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.MediumTopAppBar
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.SmallFloatingActionButton
+
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
-import androidx.compose.material3.TopAppBar
+
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
 import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.remember
+
 import androidx.compose.ui.Modifier
-import androidx.compose.runtime.mutableStateOf
+
 import com.zavedahmad.dnstoggle.ui.theme.DNSToggleTheme
 
 import com.zavedahmad.dnstoggle.viewModels.MainActivityViewModel
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.room.Room
 import com.zavedahmad.dnstoggle.data.AppDatabase
-import com.zavedahmad.dnstoggle.ui.components.ListOfDnsUi
-import com.zavedahmad.dnstoggle.ui.utilities.hasWriteSecureSettingsPermission
-import com.zavedahmad.dnstoggle.ui.utilities.turnOffPrivateDns
+
 import androidx.lifecycle.ViewModelProvider
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
+
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.platform.LocalUriHandler
 import androidx.compose.ui.res.painterResource
@@ -56,10 +50,10 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
 import androidx.lifecycle.ViewModel
 import com.zavedahmad.dnstoggle.R
-import com.zavedahmad.dnstoggle.data.DnsDomainEntry
+
 import com.zavedahmad.dnstoggle.ui.components.DNSAddDialogueUI
 import com.zavedahmad.dnstoggle.ui.pages.MainPage
-import kotlin.toString
+
 
 @OptIn(ExperimentalMaterial3Api::class)
 class MainActivity : ComponentActivity() {
@@ -88,7 +82,7 @@ class MainActivity : ComponentActivity() {
                 Scaffold(
                     modifier = Modifier.Companion.fillMaxSize().nestedScroll(scrollBehaviour.nestedScrollConnection),
                     floatingActionButton = {
-                        FloatingActionButton(onClick = { viewModel.showDNSDialogue() }, modifier = Modifier.offset(x=-20.dp,y=-50.dp), containerColor = MaterialTheme.colorScheme.secondary , contentColor = MaterialTheme.colorScheme.onSecondary) {
+                        FloatingActionButton(onClick = { viewModel.showDNSDialogue() }, modifier = Modifier.offset(x=(-20).dp,y=(-50).dp), containerColor = MaterialTheme.colorScheme.secondary , contentColor = MaterialTheme.colorScheme.onSecondary) {
                             Icon(
                                 painter = painterResource(id = R.drawable.baseline_add_24),
                                 contentDescription = "add New DNS"
@@ -103,7 +97,7 @@ class MainActivity : ComponentActivity() {
                                 titleContentColor = MaterialTheme.colorScheme.onPrimaryContainer,
                             ),
                             title = {
-                                Text("PrivaeDnsToggle")
+                                Text("PrivateDnsToggle")
                             },
                             actions = {
                                 IconButton(onClick = { uriHandler.openUri("http://www.github.com/zaved707/") }) {
